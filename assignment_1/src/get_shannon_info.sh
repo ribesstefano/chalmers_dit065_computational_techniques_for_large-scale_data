c7919de5ce873368f7a5c442c204da70427b52d1
Stefano Ribes <ribes@bayes.ita.chalmers.se>
2022-03-27 20:00:17 +0200

    Assignment1: Written scripts for collecting servers info

---
+++ assignment_1/src/get_shannon_info.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --partition=cpu-shannon
+#SBATCH --cpus-per-task=1
+#SBATCH --output=/bayes_datainbackup/home/2022/ribes/chalmers_dit065_computational_techniques_for_large-scale_data/assignment_1/src/serverinfo_shannon.out
+#SBATCH --error=/bayes_datainbackup/home/2022/ribes/chalmers_dit065_computational_techniques_for_large-scale_data/assignment_1/src/serverinfo_shannon.error
+#SBATCH --chdir=/bayes_datainbackup/home/2022/ribes/chalmers_dit065_computational_techniques_for_large-scale_data/assignment_1/src # Working directory
+#SBATCH --export=ALL,TEMP=/scratch,TMP=/scratch,TMPDIR=/scratch
+echo "================================================================================"
+echo "Number of CPU cores"
+echo "================================================================================"
+lscpu | egrep 'CPU\(s\)|Core|Socket|Thread'
+echo "================================================================================"
+echo "CPU Type"
+echo "================================================================================"
+lscpu | egrep "Model name| MHz"
+echo "================================================================================"
+echo "Disk Amount"
+echo "================================================================================"
+df -h --total
+echo "================================================================================"
+echo "Login shell memory"
+echo "================================================================================"
+ps auxU ribes | awk '{memory +=$4}; END {print memory }'
