bb4b64b656fddc1dd4ad80759801150910eb5a2f
Cem Mert Dallı <30482236+cmertdalli@users.noreply.github.com>
2022-04-17 11:50:19 +0200

    Add files via upload

---
+++ assignment_3/src/problem2a.py
@@ -0,0 +1,26 @@
+from mrjob.job import MRJob
+from matplotlib.pyplot import hist
+from time import process_time
+import numpy as np
+
+class Summary(MRJob):
+    def mapper(self, _, line):
+        _, _, number = line.split()
+        yield 'value', float(number)
+
+    def reducer(self, _, numbers):
+        numbers = np.fromiter(numbers, dtype=float)
+ 
+        yield 'minimum', np.min(numbers)
+        yield 'maximum', np.max(numbers)
+        yield ('mean', np.mean(numbers))
+        yield ('standard_deviation', np.std(numbers))
+
+        bins, histogram, _ = hist(numbers, bins=10)
+        yield 'hist', (bins.tolist(), histogram.tolist())
+
+if __name__ == '__main__':
+    start = process_time()
+    Summary.run()
+    processing_time = process_time() - start
+    print('time: {} s'.format(processing_time))
