5371ffdabe2ed7f70376c628ab432d5eb8e8b1e2
Huw Fryer <huwfryer@gmail.com>
2022-04-19 21:54:52 +0200

    Change to median estimate for 2c

--- assignment_3/src/problem2c.py
+++ assignment_3/src/problem2c.py
@@ -1,19 +1,25 @@
 from mrjob.job import MRJob
-from matplotlib.pyplot import hist
 from time import process_time
-import numpy as np
+
 
 class Summary(MRJob):
+    SORT_VALUES = True
+
     def mapper(self, _, line):
         _, _, number = line.split()
         yield 'value', float(number)
 
+    def combiner(self, key, numbers):
+        # numbers = np.fromiter(numbers, dtype=float)
+        numbers = tuple(numbers)
+        yield ('median', numbers[len(numbers) // 2])
+
     def reducer(self, _, numbers):
-        numbers = np.fromiter(numbers, dtype=float)
-        yield ('median', np.median(numbers))
+        # numbers = np.fromiter(numbers, dtype=float)
+        numbers = tuple(numbers)
+        yield ('median', numbers[len(numbers) // 2])
+
 
 if __name__ == '__main__':
     start = process_time()
     Summary.run()
-    processing_time = process_time() - start
-    print('time: {} s'.format(processing_time))
